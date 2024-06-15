85bdcc89e6fe86fc56c99397ef5b02f6b8a19c42
violeteisa <violeteisa1999@gmail.com>
2024-06-15 09:02:03 +0300

    Second commit

--- js/index.js
+++ js/index.js
@@ -3,38 +3,42 @@ var websiteUrlInput = document.getElementById("websiteUrl");
 var searchInput = document.getElementById("searchUrl");
 var layer=document.getElementById("validationLayer");
 var closeBtn=document.getElementById("closeBtn");
-var submitBtn =document.getElementById("submitBtn")
+var submitBtn =document.getElementById("submitBtn");
 
-
-
-
-
-
-var websiteList = [];
+var websiteList = []; //array to add website 
 if (localStorage.getItem('websiteContainer') !== null) {
     websiteList = JSON.parse(localStorage.getItem('websiteContainer'));
     displayData();
 }
-
+//ADD WEBSITE FUNCTION
 function addWebsite() {
-    if( validationName()==true && validationUrl()==true){
+    if( validationName()===true && validationUrl()===true){
+
+        if (websiteUrlInput.value.indexOf('https://') !== 0) {
+            websiteUrlInput.value = 'https://' + websiteUrlInput.value;
+        }
+
         var website = {
             name: bookmarkNameInput.value,
-            Url: websiteUrlInput.value
+            Url: websiteUrl,
         };
         websiteList.push(website);
         displayData();
         localStorage.setItem('websiteContainer', JSON.stringify(websiteList));
-        clearData();
         console.log(websiteList);
     }
 }
+
+//CLEAR FUNCTION WHEN SUBMIT TO ADD ANOTHER WEBSITE
 function clearData() {
     bookmarkNameInput.value = "";
     websiteUrlInput.value = "";
     bookmarkNameInput.classList.remove("is-valid");
     websiteUrlInput.classList.remove("is-valid")
 }   
+
+
+//DISPLAY DATA ON TABLET
 function displayData() {
     var cartona = "";
     for (var i = 0; i < websiteList.length; i++) {
@@ -53,6 +57,9 @@ function displayData() {
     }
         document.getElementById("displayData").innerHTML=cartona;
 }
+
+//DELETE  FUNCTION
+//! CALL THIS FUNCTION ONCLICK DELETE BUTTON
 function deleteWebsite(indexItem){
 
     websiteList.splice(indexItem,1)
@@ -60,6 +67,9 @@ function deleteWebsite(indexItem){
     displayData();
     console.log(websiteList)
 }
+
+
+//SEARCH INPUT  
 function searchUrl(){
     var term =searchInput.value;
     var cartona = "";
@@ -81,50 +91,59 @@ function searchUrl(){
     }
     document.getElementById("displayData").innerHTML=cartona;
 }
+
+
+//NAME VALIDATION WITH REGEX
 function validationName(){
     var text = bookmarkNameInput.value;
     var regex =/^\w{3,}(\s+\w+)*$/;
     if( regex.test(text) == true){
         bookmarkNameInput.classList.add("is-valid");
         bookmarkNameInput.classList.remove("is-invalid");
-        submitBtn.addEventListener('click',function(){
-            layer.classList.replace('d-flex','d-none')
-            })
+
         return true;
     }
     else{
         bookmarkNameInput.classList.add("is-invalid");
         bookmarkNameInput.classList.remove("is-valid");
-        submitBtn.addEventListener('click',function(){
-        layer.classList.replace('d-none','d-flex')
-        })
 
         return false;
     }
 }
+
+
+//URL VALIDATION WITH REGEX
 function validationUrl(){
     var text = websiteUrlInput.value;
     var regex = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
     if( regex.test(text) == true){
         websiteUrlInput.classList.add("is-valid");
         websiteUrlInput.classList.remove("is-invalid");
-        submitBtn.addEventListener('click',function(){
-            layer.classList.replace('d-flex','d-none')
-            })
     
         return true;
     }
     else{
         websiteUrlInput.classList.add("is-invalid");
         websiteUrlInput.classList.remove("is-valid");
-        submitBtn.addEventListener('click',function(){
-            layer.classList.replace('d-none','d-flex')
-            })
         return false;
     }
 }
 
-closeBtn.addEventListener('click', function(){
+
+//SUBMIT BUTTON
+    submitBtn.addEventListener('click', function () {
+        if (validationName() === false || validationUrl() === false) {
+            layer.classList.replace('d-none', 'd-flex');
+        } else {
+            layer.classList.replace('d-flex', 'd-none');
+            clearData();
+        }
+    });
+
+
+//CLOSE BUTTON INTO MODAL   
+closeBtn.addEventListener('click', function(e){
+    e.preventDefault();
     layer.classList.replace('d-flex','d-none')
 })
 
